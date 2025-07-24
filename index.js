e0c238242913cb534659429b3e1be7027176431a
CreazyBryon <hsmsyy2003@163.com>
2025-07-24 11:10:57 +0800

    add skidata specific url

--- index.js
+++ index.js
@@ -29,6 +29,43 @@ function basicAuth(req, res, next) {
   return res.status(401).send('Invalid credentials.');
 }
 
+//skidata specific==========================================================================================
+app.post('/data/v1/postEvent',basicAuth, async (req, res) => {
+  try {
+    const files = await fs.readdir(DATA_DIR);
+    const jsonFiles = files.filter(f => f.endsWith('.json'));
+    if (jsonFiles.length >= 100) {
+      return res.status(429).json({ error: 'File limit reached. Cannot receive more data.' });
+    }
+
+    const jsonData = req.body;
+    const headers = req.headers;
+    const now = new Date();
+    // Format: YYYY-MM-DD_HH-mm-ss
+    const timestamp = now.toISOString().replace(/[:.]/g, '');
+    const localTime = now.toLocaleString();
+    const filename = `recv_${timestamp}.json`;
+    const filepath = path.join(DATA_DIR, filename);
+    const toSave = { "request_local_time":localTime, "request_headers":headers, "request_body": jsonData };
+
+    await fs.writeFile(filepath, JSON.stringify(toSave, null, 2), 'utf-8');
+    res.json({ message: 'Data and headers saved', filename });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to save file' });
+  }
+});
+
+// 2. GET /list: list all received JSON files
+app.get('/data/v1/health', async (req, res) => {
+  try {
+    res.json({"online":"up"});
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to list files' });
+  }
+});
+
+//end of skidata========================================================================================
+
 // 1. POST /data: receive any JSON, save to file by time
 app.post('/data',basicAuth, async (req, res) => {
   try {
